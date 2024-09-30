9c27a5707d9ad474feb0feec9e4926916f20ac29
tatangharyadi <tatang.haryadi@outlook.com>
2024-10-01 04:01:19 +0700

    refactor: case study

--- js/script.js
+++ js/script.js
@@ -7,3 +7,35 @@ const texts = {
 function handleOnClick(key) {
     document.getElementById('hero-text').innerText = texts[key];
 }
+
+let nextButton = document.getElementById('next');
+let prevButton = document.getElementById('prev');
+let carousel = document.querySelector('.casestudy--container');
+let listHTML = document.querySelector('.casestudy--list');
+
+nextButton.onclick = function() {
+    showSlider(next);
+}
+prevButton.onclick = function() {
+    showSlider(prev);
+}
+
+let unAcceptClick;
+const showSlider = (type) => {
+    nextButton.style.pointerEvents = 'none';
+    prevButton.style.pointerEvents = 'none';
+
+    let items = document.querySelectorAll('.casestudy--item');
+    if(type === next) {
+        listHTML.appendChild(items[0]);
+    } else {
+        let positionLast = items.length - 1;
+        listHTML.prepend(items[positionLast]);
+    }
+
+    clearTimeout(unAcceptClick);
+    unAcceptClick = setTimeout(() => {
+        nextButton.style.pointerEvents = 'auto';
+        prevButton.style.pointerEvents = 'auto';
+    }, 1000);
+}
